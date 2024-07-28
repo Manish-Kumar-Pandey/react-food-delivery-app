247a245c400397285f1905d204688d9c3fbcf079
Manish Kumar Pandey <diwakarpandey767@gmail.com>
2024-07-28 17:33:12 +0530

    Added css to cart total amount

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -26,9 +26,9 @@ const Cart = () => {
                         <div className="cart-items-title cart-items-item">
                         <img src={item.image} alt="" />
                         <p>{item.name}</p>
-                        <p>{item.price}</p>
+                        <p>${item.price}</p>
                         <p>{cartItems[item._id]}</p>
-                        <p>{item.price*cartItems[item._id]}</p>
+                        <p>${item.price*cartItems[item._id]}</p>
                         <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
                     </div>
                     <hr/>
@@ -36,6 +36,37 @@ const Cart = () => {
                 )
             }
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+            <h2>Cart Total</h2>
+            <div>
+                <div className="cart-total-details">
+                    <p>Subtotal</p>
+                    <p>{0}</p>
+                </div>
+                <hr />
+                <div className="cart-total-details">
+                    <p>Delivery Fee</p>
+                    <p>{2}</p>
+                </div>
+                <hr />
+                <div className="cart-total-details">
+                    <p>Total</p>
+                    <p>{0}</p>
+                </div>
+            </div>
+            <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+            <div>
+                <p>If you have a promo code, Enter it here</p>
+                <div className="cart-promocode-input">
+                    <input type="text" placeholder='promo code'/>
+                    <button>Submit</button>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   )
