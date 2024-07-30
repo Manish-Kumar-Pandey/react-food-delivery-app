dbe8014b3e937b496905eb72a066e7edb4b5f8fe
Manish Kumar Pandey <diwakarpandey767@gmail.com>
2024-07-30 17:33:49 +0530

    added admin panel code

---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Orders.css'
+
+const Orders = () => {
+  return (
+    <div>
+        orders
+    </div>
+  )
+}
+import './Orders.css'
+
+export default Orders
