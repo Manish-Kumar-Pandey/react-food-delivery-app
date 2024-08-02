abee48b036b69e815f9ecb96d50bc90609fa6650
Manish-Kumar-Pandey <62229650+Manish-Kumar-Pandey@users.noreply.github.com>
2024-08-02 18:36:39 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://react-food-delivery-app-backend.onrender.com"
 
   return (
     <div>
